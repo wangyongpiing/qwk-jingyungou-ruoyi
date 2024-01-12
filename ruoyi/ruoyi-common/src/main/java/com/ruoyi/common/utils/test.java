c79810bb8ef2c8774b2f64cf6f7b17df9e65cb6d
wangyongping <2019369925@qq.com>
2024-01-12 15:58:59 +0800

    测试上传

---
+++ ruoyi/ruoyi-common/src/main/java/com/ruoyi/common/utils/test.java
@@ -0,0 +1,15 @@
+package com.ruoyi.common.utils;/**
+ * @Description:
+ * @author cjf
+ * @date 2024-01-12
+ */
+
+/**
+ * @author cjf
+ * @date 2024/1/12
+ */
+public class test {
+    public static void main(String[] args) {
+        //测试代码
+    }
+}
