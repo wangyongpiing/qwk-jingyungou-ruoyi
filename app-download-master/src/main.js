9c9a4cb88152a26dfd1518718c1a11e12af7441e
wangyongping <2019369925@qq.com>
2024-01-12 11:49:58 +0800

    vue/下载生肖宝

---
+++ app-download-master/src/main.js
@@ -0,0 +1,18 @@
+import 'amfe-flexible'
+import './styles/index.scss'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import Vue from 'vue'
+import App from './App.vue'
+import ElementUI from 'element-ui'
+import axios from 'axios'
+Vue.prototype.$axios = axios
+
+
+Vue.use(ElementUI)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
